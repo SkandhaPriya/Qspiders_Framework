6f12b77a4953627732f9f428c526a1096e3a95cb
gbalu <gbalu@Sathish>
2024-04-06 13:11:42 +0530

    Push to repository

---
+++ Github_Launching_URL/src/Github_pk/URL_LAUNCHING.java
@@ -0,0 +1,16 @@
+package Github_pk;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class URL_LAUNCHING 
+{
+public static void main(String[] args) 
+{
+	System.setProperty("webdriver.gecko.driver", "./softwares/geckodriver.exe");
+	WebDriver driver = new FirefoxDriver();
+	driver.get("https://www.facebook.com");	
+	driver.quit();
+}
+}
+
